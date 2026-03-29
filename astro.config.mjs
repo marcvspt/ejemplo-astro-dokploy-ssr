ce80eb58d764af56426cf46b6c4705c608ef379c
marcvspt <78035694+marcvspt@users.noreply.github.com>
2026-03-29 00:59:57 -0600

    Añadir adaptador de node

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,11 +3,22 @@ import { defineConfig } from 'astro/config';
 
 import tailwindcss from '@tailwindcss/vite';
 
+import node from '@astrojs/node';
+
 // https://astro.build/config
 export default defineConfig({
   output: "server",
 
   vite: {
     plugins: [tailwindcss()]
+  },
+
+  adapter: node({
+    mode: 'standalone'
+  }),
+
+  server: {
+    host: true,
+    port: 3000 // Ahora se usará http://localhost:3000
   }
 });
